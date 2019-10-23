63543743cf4da9ae31c48b0ed295fc96c5e71e55
Oskar Stenberg <01ste02@utb.lund.se>
2019-10-23 11:20:38 +0200

    Initial

--- JumpingBall/MovingCanvas.cs
+++ JumpingBall/MovingCanvas.cs
@@ -13,21 +13,21 @@ using Android.Widget;
 
 namespace JumpingBall
 {
-    class MovingCanvas
+    public class MovingCanvas : View
     {
         MovingPicture b;
         Context c;
 
         public MovingPicture B { get => b; set => b = value; }
 
-        public MovingCanvas(Context context, IAttributeSet attrs): base()
+        public MovingCanvas(Context context, IAttributeSet attrs): base(context, attrs)
         {
             Initialize();
             c = context;
         }
 
         public MovingCanvas(Context context, IAttributeSet attrs, int defStyle):
-            base()
+            base(context, attrs, defStyle)
         {
             Initialize();
             c = context;
@@ -38,25 +38,9 @@ namespace JumpingBall
 
         }
 
-        protected override void OnMeasure(int w, int h)
+        protected override void OnMeasure (int w, int h)
         {
-            base.OnMeasure(w, h);
-            this.SetMeasuredDimension(MeasureSpec.GetSize(w), MeasureSpec.GetSize(h));
-            this.B = new MovingCanvas(Height / 2, 10);
-
-        }
-
-        protected override void OnDraw(Canvas canvas)
-        {
-            this.SetBackgroundColor(Color.White);
-            Paint p = new Paint();
-            canvas.DrawBitmap(BitmapFactory.DecodeResource(c.Resources, Resource.Drawable.ball), Width / 2, B.Y, p);
-            FontMetrics fm = p.GetFontMetrics();
-            p.Color = Color.Black;
-            p.StrokeWidth = 2;
-            p.TextSize = 24;
-            canvas.DrawText(b.NrJump + " jumps", 10, Height / 2 - (fm.Ascent + fm.Descent) / 2, p);
-
+            base.OnMeasure
         }
 
     }
