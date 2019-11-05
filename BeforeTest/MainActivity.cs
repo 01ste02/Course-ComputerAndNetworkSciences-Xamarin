07ea5badd19c559d6b32874f63b4cca1f731baa5
Oskar Stenberg <01ste02@utb.lund.se>
2019-11-05 10:12:31 +0100

    BeforeTest

---
+++ BeforeTest/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using static Android.Views.View;
+using Android.Content;
+
+namespace BeforeTest
+{
+    [Activity(Label = "BeforeTest", MainLauncher = true)]
+    public class MainActivity : Activity, IOnClickListener
+    {
+        Button btn;
+        EditText num2;
+        EditText num1;
+
+        public void OnClick(View v)
+        {
+            if (v.Id == btn.Id)
+            {
+                int number1 = int.Parse(num2.Text);
+                int number2 = int.Parse(num1.Text);
+
+                int added = number1 + number2;
+
+                Intent intent = new Intent(this, typeof(Second));
+                intent.PutExtra("addedNum", added);
+
+                StartActivity(intent);
+            }
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            btn = FindViewById<Button>(Resource.Id.button1);
+            num2 = FindViewById<EditText>(Resource.Id.editText1);
+            num1 = FindViewById<EditText>(Resource.Id.editText2);
+
+            btn.SetOnClickListener(this);
+        }
+    }
+}
+
---
+++ BeforeTest/Second.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace BeforeTest
+{
+    [Activity(Label = "Second")]
+    public class Second : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.layout_2);
+
+            int added = Intent.GetIntExtra("addedNum", 0);
+
+            TextView txt = FindViewById<TextView>(Resource.Id.textView1);
+
+            txt.Text = added.ToString();
+
+            // Create your application here
+        }
+    }
+}
